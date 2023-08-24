7acb08f45a0305a0c5485ef64229f7eec8425425
Judee ジ <115565273+JudeeFr@users.noreply.github.com>
2023-08-24 17:48:52 +0200

    transactions collapse

--- argentbank-app/src/components/collapse/index.js
+++ argentbank-app/src/components/collapse/index.js
@@ -1,28 +1,71 @@
-import React  from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
+import arrowDown from '../../assets/img/arrowDown.png'
 
+export default function Collapse({date, description, amount, balance, type, category, note}) {
+    const [selected, setSelected] = useState(false);
+	const [firstClick, setFirstClick] = useState(false);
 
-export default function Collapse({date, description, amount, balance}) {
+	useEffect(() => {
+		// on initialise le firstClick a true au moment ou on detecte le premier click
+		if (selected && !firstClick) {
+			setFirstClick(true);
+		}
+	}, [selected]);
+   
+   
     return (
-        <div>
+        <div className='collapse-grid'>
         
-        <div className='transactions-grid'>    
-            <div className='col-1'>
+        <div className='collapse-title'>    
+            <div className='collapse-col-1'>
                 {date}
             </div>
-            <div className='col-1'> 
+            <div className='collapse-col-2'> 
                 {description}
             </div>
-            <div className='col-2'>
+            <div className='collapse-col-3'>
                 {amount}
             </div>
-            <div className='col-3'>
+            <div className='collapse-col-4'>
                 {balance}
             </div>
-            <div className='col-4'>
-                
+            <div className='collapse-col-5'>
+            <img
+					src={arrowDown}
+					alt="arrow"
+					className={`collapse-arrow ${selected ? 'rotate' : 'rotate2'}`}
+					onClick={() => setSelected(!selected)}
+				/>
             </div>
         </div>
+
+        <div style={{ display: !firstClick ? 'none' : '' }} 
+        className={`collapse-text ${selected ? 'collapse-open' : 'collapse-close'}`}>
+            <div className='col-1'>
+                <div>Transaction type</div>
+                <div>Category</div>
+                <div>Note</div>
+            </div>
+            <div className='col-2'> 
+                <div>
+                    {type}
+                </div>
+                <div className='category'>
+                    {category}
+                    <i className="fa-solid fa-pencil"></i>
+                </div>
+                <div className='note'>
+                    {note}
+                    <i className="fa-solid fa-pencil"></i>
+                </div>
+            </div>
+            <div className='col-3'></div>
+            <div className='col-4'></div>
+            <div className='col-5'></div>
+            
+        </div>
+
         </div>
     );
   }
--- argentbank-app/src/pages/transactions.js
+++ argentbank-app/src/pages/transactions.js
@@ -12,7 +12,11 @@ export default function Transactions() {
         <Header/>
       </div>      
       <div>
-        <Account/>
+        <Account
+        title="Argent Bank Checking (x8349)" 
+        amount="$2,082.79" 
+        description="Available Balance"
+        />
       </div>   
       <div className='grid'>
         <div className='col_1'>
@@ -27,13 +31,54 @@ export default function Transactions() {
         <div className='col_4'>
           Balance
         </div>
+        <div className='col_5'>
+        </div>
       </div>
       <div>
         <Collapse
-        date={""}
-        description={""}
-        amount={""}
-        balance={""}
+        date="27/02/20"
+        description="Golden Sun Backery"
+        amount="$8.00"
+        balance="$298.00"
+        type="Electronic"
+        category="Food"
+        note="Lorem Ipsum"
+        />
+         <Collapse
+        date="27/02/20"
+        description="Golden Sun Backery"
+        amount="$8.00"
+        balance="$298.00"
+        type="Electronic"
+        category="Food"
+        note="Lorem Ipsum"
+        />
+         <Collapse
+        date="27/02/20"
+        description="Golden Sun Backery"
+        amount="$8.00"
+        balance="$298.00"
+        type="Electronic"
+        category="Food"
+        note="Lorem Ipsum"
+        />
+         <Collapse
+        date="27/02/20"
+        description="Golden Sun Backery"
+        amount="$8.00"
+        balance="$298.00"
+        type="Electronic"
+        category="Food"
+        note="Lorem Ipsum"
+        />
+         <Collapse
+        date="27/02/20"
+        description="Golden Sun Backery"
+        amount="$8.00"
+        balance="$298.00"
+        type="Electronic"
+        category="Food"
+        note="Lorem Ipsum"
         />
       </div>
       <div>  
